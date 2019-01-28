2f9ff92b617dde71d60c1e5207f3e67d1f01e48d
David Ben Knoble <ben.knoble@gmail.com>
2019-01-27 19:22:59 -0500

    Demonstrate 'debug' script with assertion

--- src/nix/etc/Util.java
+++ src/nix/etc/Util.java
@@ -4,6 +4,7 @@ public class Util {
     private static final String greeting = "Hello";
 
     public static String makeGreeting(String name) {
+        assert false : "This should only be an error in 'debug' mode";
         return Util.greeting + " " + name;
     }
 }
